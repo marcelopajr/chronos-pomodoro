67db1665857a225bbbd1284b658fd205eec3fc26
Marcelo Pereira <marcelo96jr@gmail.com>
2025-05-26 18:44:29 +0100

    chore: config task context logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { TaskContextProvider } from './contexts/TaskContext/TaskContextProvider';
 import { Home } from './pages/Home';
 
 import './styles/theme.css';
 import './styles/global.css';
 
 export function App() {
-  return <Home />;
+  return (
+    <TaskContextProvider>
+      <Home />
+    </TaskContextProvider>
+  );
 }
