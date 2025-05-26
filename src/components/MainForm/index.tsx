2f140c5a610c863349676945d5caf246beae64bb
Marcelo Pereira <marcelo96jr@gmail.com>
2025-05-26 19:43:59 +0100

    feat: add handle create new task function

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -1,16 +1,55 @@
+import { useRef } from 'react';
 import { PlayCircleIcon } from 'lucide-react';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
+import type { TaskModel } from '../../models/TaskModel';
 import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
 
 export function MainForm() {
+  const { setState } = useTaskContext();
+  const taskNameInput = useRef<HTMLInputElement>(null);
+
+  function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    if (taskNameInput.current === null) return;
+
+    const taskName = taskNameInput.current.value.trim();
+
+    if (!taskName) {
+      alert('Task name is required');
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: String(Date.now()),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptedDate: null,
+      duration: 1,
+      type: 'workTime',
+    };
+
+    setState(prevState => ({
+      ...prevState,
+      activeTask: newTask,
+      currentCycle: prevState.currentCycle + 1,
+      secondsRemaining: newTask.duration * 60,
+      formattedSecondsRemaining: '00:00',
+      tasks: [...prevState.tasks, newTask],
+    }));
+  }
+
   return (
-    <form className='form' action=''>
+    <form onSubmit={handleCreateNewTask} className='form' action=''>
       <div className='formRow'>
         <DefaultInput
           id='task'
           labelText='Task'
           placeholder='Write something'
+          ref={taskNameInput}
         />
       </div>
 
